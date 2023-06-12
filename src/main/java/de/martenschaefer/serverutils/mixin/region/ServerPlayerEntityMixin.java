94cbeab84f57f105702b170d63e2e5cdd9e2aa97
mschae23 <46165762+mschae23@users.noreply.github.com>
2023-06-12 19:20:31 +0200

    Potentially fixed PvP protection rule

--- src/main/java/de/martenschaefer/serverutils/mixin/region/ServerPlayerEntityMixin.java
+++ src/main/java/de/martenschaefer/serverutils/mixin/region/ServerPlayerEntityMixin.java
@@ -1,6 +1,6 @@
 package de.martenschaefer.serverutils.mixin.region;
 
-import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ActionResult;
 import de.martenschaefer.serverutils.region.RegionRuleEnforcer;
@@ -11,14 +11,14 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerEntityMixin {
-    @Redirect(method = "damage", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;isPvpEnabled()Z"))
-    private boolean redirectIsPvpEnabled(ServerPlayerEntity player, DamageSource source) {
+    @Redirect(method = "damage", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;shouldDamagePlayer(Lnet/minecraft/entity/player/PlayerEntity;)Z"))
+    private boolean redirectIsPvpEnabled(ServerPlayerEntity player, PlayerEntity attacker) {
         ActionResult result = RegionRuleEnforcer.onPlayerPvp(player, player.getPos());
 
         if (result == ActionResult.FAIL) {
             return false;
-        } else if (source.getAttacker() instanceof ServerPlayerEntity attacker) {
-            result = RegionRuleEnforcer.onPlayerPvpSendDenied(attacker, attacker.getPos());
+        } else if (attacker instanceof ServerPlayerEntity serverAttacker) {
+            result = RegionRuleEnforcer.onPlayerPvpSendDenied(serverAttacker, attacker.getPos());
 
             if (result == ActionResult.FAIL) {
                 return false;
